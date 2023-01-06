c5064c22aff81a37256845246553d75521789948
Jeremiah <jeremiahaimiton@gmail.com>
2023-01-06 12:46:27 +0100

    local storage

---
+++ textUpdate.js
@@ -0,0 +1,38 @@
+/** @format */
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const ulEl = document.getElementById("ul-el");
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+deleteBtn.addEventListener("click", function () {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+});
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
+});
+
+function renderLeads() {
+  let listItems = "";
+
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+    <li> 
+  
+    ${myLeads[i]}
+    <li/>`;
+  }
+  ulEl.innerHTML = listItems;
+}
